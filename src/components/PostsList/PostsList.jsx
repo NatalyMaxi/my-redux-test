51b0ce4a4c85f8dad7d4dae6af7a0d370190d71a
Natalya Maximova <natka-31@mail.ru>
2023-06-22 12:10:24 +0500

    add hook

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { postDelete } from '../../redux/posts/postsSlice';
 
 import { Link } from 'react-router-dom';
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 
 import AddPostForm from '../AddPostForm/AddPostForm';
 import PostAuthor from '../PostAuthor/PostAuthor';
@@ -12,6 +12,8 @@ import TimeAgo from '../TimeAgo/TimeAgo';
 import ReactionButtons from '../ReactionButtons/ReactionButtons';
 import FilteringPosts from '../FilteringPosts/FilteringPosts';
 import MyModal from '../UI/MyModal/MyModal';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { usePosts } from '../../hooks/usePosts';
 
 const PostsList = () => {
   const [filter, setFilter] = useState({ sort: '', query: '' })
@@ -19,16 +21,7 @@ const PostsList = () => {
   const posts = useSelector(state => state.posts)
   const dispatch = useDispatch()
 
-  const orderedPosts = useMemo(() => {
-    if (filter.sort) {
-      return posts.slice().sort((a, b) => b[filter.sort].localeCompare(a[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return orderedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, orderedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   return (
     <section className={classes.postsPage}>
@@ -43,42 +36,52 @@ const PostsList = () => {
         {sortedAndSearchedPosts.length !== 0
           ? <div>
             <h2 className={classes.title}>Posts</h2>
-            {
-              sortedAndSearchedPosts.map(post => (
-                <article className={classes.post} key={post.id}>
-                  <div className={classes.postItems}>
-                    <h3 className={classes.postTitle}>{post.title}</h3>
-                    <div className={classes.postItem}>
-                      <PostAuthor userId={post.user} />
-                      <TimeAgo timestamp={post.date} />
-                    </div>
-                  </div>
-                  <p className={classes.postContent}>{post.content.substring(0, 130)}</p>
-                  <div className={classes.postItems}>
-                    <ReactionButtons post={post} />
-                    <div className={classes.buttonContainer}>
-                      <Link to={`/posts/${post.id}`}
-                        className={classes.viewButton}>
-                        <button
-                          className={classes.button}
-                          type='button'
-                        >
-                          View Post
-                        </button>
-                      </Link>
-                      <button
-                        className={classes.button}
-                        type='button'
-                        onClick={() => {
-                          dispatch(postDelete(post))
-                        }}>
-                        Delete Post
-                      </button>
-                    </div>
-                  </div>
-                </article>
-              ))
-            }
+            <TransitionGroup>
+              {
+                sortedAndSearchedPosts.map(post => (
+                  <CSSTransition
+                    key={post.id}
+                    timeout={500}
+                    classNames={{ ...classes }}
+                  >
+                    <article className={classes.post}>
+                      <div className={classes.postItems}>
+                        <h3 className={classes.postTitle}>{post.title}</h3>
+                        <div className={classes.postItem}>
+                          <PostAuthor userId={post.user} />
+                          <TimeAgo timestamp={post.date} />
+                        </div>
+                      </div>
+                      <p className={classes.postContent}>{post.content.substring(0, 130)}</p>
+                      <div className={classes.postItems}>
+                        <ReactionButtons post={post} />
+                        <div className={classes.buttonContainer}>
+                          <Link to={`/posts/${post.id}`}
+                            className={classes.viewButton}>
+                            <button
+                              className={classes.button}
+                              type='button'
+                            >
+                              View Post
+                            </button>
+                          </Link>
+                          <button
+                            className={classes.button}
+                            type='button'
+                            onClick={() => {
+                              dispatch(postDelete(post))
+                            }}>
+                            Delete Post
+                          </button>
+                        </div>
+                      </div>
+                    </article>
+                  </CSSTransition>
+
+                ))
+              }
+            </TransitionGroup>
+
           </div>
           :
           <h3 className={classes.title}>Posts are missing</h3>
